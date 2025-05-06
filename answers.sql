12a37550004b35d6fc6c545ed0e2162a90022c78
Alora <misshie21@gmail.com>
2025-05-06 16:20:58 +0200

    Assignment Redo

--- answers.sql
+++ answers.sql
@@ -1,24 +1,43 @@
--- Question 1
--- STRING_SPLIT to normalize to 1NF
 
-SELECT 
-    OrderID,
-    CustomerName,
-    LTRIM(RTRIM(value)) AS Product
-FROM 
-    ProductDetail
-CROSS APPLY 
-    STRING_SPLIT(Products, ',');
-
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(01, 'Themba Moyo', 'Laptop'),
+(01, 'Themba Moyo', 'Mouse'),
+(02, 'Ruth Ndlovu', 'Tablet'),
+(02, 'Ruth Ndlovu', 'Keyboard'),
+(02, 'Ruth Ndlovu', 'Mouse'),
+(03, 'Kelly Tom', 'Phone');
 -- Question 2
--- Normalisation to 2NF ends with 2 tables extracted from the main table:
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(01, 'Themba Moyo'),
+(02, 'Ruth Ndlovu'),
+(03, 'Kelly Tom');
 
---Create the Orders table
-SELECT DISTINCT OrderID, CustomerName
-INTO Orders
-FROM OrderDetails;
 
--- Create the OrderItems table
-SELECT OrderID, Product, Quantity
-INTO OrderItems
-FROM OrderDetails;
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(01, 'Laptop', 2),
+(01, 'Mouse', 1),
+(02, 'Tablet', 3),
+(02, 'Keyboard', 1),
+(02, 'Mouse', 2),
+(03, 'Phone', 1);
+
